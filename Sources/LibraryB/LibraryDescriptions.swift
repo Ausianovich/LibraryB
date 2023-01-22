b495ae2f523cbcb3aa85d1ec3edefe3ea03d86cd
Ausianovich Kanstantsin <smilekostik@yandex.ru>
2023-01-22 18:58:25 +0200

    Add Library A dependency into LibraryB

---
+++ Sources/LibraryB/LibraryDescriptions.swift
@@ -0,0 +1,33 @@
+//
+//  LibraryDescriptions.swift
+//  
+//
+//  Created by Kanstantsin Ausianovich on 22.01.23.
+//
+
+import Foundation
+import LibraryA
+
+public final class LibraryBDescriptions {
+    
+    static var shared: LibraryBDescriptions = {
+        let instance = LibraryBDescriptions()
+        
+        return instance
+    }()
+    
+    private init() {}
+    
+    func printLibraryBName() {
+        print("---!!!--- This is LIBRARY B")
+        
+        LibraryDescriptions.shared.printLibraryName()
+        
+    }
+}
+
+extension LibraryBDescriptions: NSCopying {
+    public func copy(with zone: NSZone? = nil) -> Any {
+        return self
+    }
+}
